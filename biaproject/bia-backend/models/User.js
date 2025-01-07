1a9cc1d7a3837552009222710b89d9bc63edfdf7
hatemkilani <hatem.kilani1999l@gmail.com>
2025-01-08 00:30:28 +0100

    Removed biaproject as a submodule

--- biaproject/bia-backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, unique: true, sparse: true }, // Add sparse to allow multiple null values
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model('User', userSchema);
--- biaproject/src/app/lottie.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@lottiefiles/react-lottie-player';
